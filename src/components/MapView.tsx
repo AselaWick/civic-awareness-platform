b8df8fbceba0d0d5d65e7293958d71210076a1bf
AselaWick <thilanka20000@gmail.com>
2025-09-06 19:51:02 +0400

    mapShow1.json

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -14,18 +14,37 @@ interface MapViewProps {
   issues: Issue[];
 }
 
+const ResizeFix = () => {
+  const map = useMap();
+  useEffect(() => {
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 300);
+  }, [map]);
+  return null;
+};
+
 const MapView = ({ issues }: MapViewProps) => {
   const center: LatLngExpression = [23.6, 58.5]; // Default center (Oman)
 
   return (
-    <div className="h-[500px] w-full rounded shadow-md overflow-hidden">
-      <MapContainer center={center} zoom={6} scrollWheelZoom={true} className="h-full w-full z-0">
+    <div className="relative w-full h-[500px] rounded shadow-md overflow-hidden">
+      <MapContainer
+        center={center}
+        zoom={6}
+        scrollWheelZoom={true}
+        className="absolute inset-0 z-0"
+      >
+        <ResizeFix />
         <TileLayer
           attribution='&copy; OpenStreetMap contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {issues.map(issue => (
-          <Marker key={issue.id} position={[issue.location.lat, issue.location.lng]}>
+          <Marker
+            key={issue.id}
+            position={[issue.location.lat, issue.location.lng]}
+          >
             <Popup>
               <strong>{issue.title}</strong><br />
               {issue.description}
