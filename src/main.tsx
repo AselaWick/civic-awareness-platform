0fac69f2141be901578314eadd23fde9012c5f56
AselaWick <thilanka20000@gmail.com>
2025-09-06 21:25:06 +0400

    Updatemail.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MapTestRouter from './test/MapTestRouter'; // ✅ not App
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MapTestRouter />
+  </React.StrictMode>
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
