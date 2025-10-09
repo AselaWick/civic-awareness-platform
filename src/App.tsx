e91939661f395733af28e0d18e669b16ec8dffbd
AselaWick <thilanka20000@gmail.com>
2025-10-09 12:15:03 +0400

    Omit Login Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './assets/auth/Login';
+// import Login from './assets/auth/Login'; ← Login route disabled
 
 import './App.css';
 import { useState } from 'react';
@@ -63,7 +63,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<CivicHome />} />
-      <Route path="/login" element={<Login />} />
+      {/* <Route path="/login" element={<Login />} /> ← Temporarily disabled */}
     </Routes>
   );
 }
